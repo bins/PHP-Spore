eab4298b1138afd1fbdd5230c5b933a42c99378b
flechau- <frederic@weborama.com>
2012-11-15 10:08:11 +0100

    update the routes and the function used

--- bin/spore_auth.php
+++ bin/spore_auth.php
@@ -2,13 +2,14 @@
 
 require_once '../lib/Spore.php';
 
-$spore = new Spore('../../../authentication/spore.yaml');
+//$spore = new Spore('../../../authentication/spore.yaml');
+$spore = new Spore('../config/route_config.desktop.yaml');
 
 $spore->setCookie("webo_auth", "634322571564233712030311395365302871");
 
-$result = $spore->get_user();
+$result = $spore->get_user(array('id' => 1, 'format' => 'json'));
 
-print_r(result);
+print_r($result);
 
 $result2 = $spore->create_user(array('user_email' => "voicibin@gmail.com"));
 
